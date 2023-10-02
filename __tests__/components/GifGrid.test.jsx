4f2cd11d8b278d14ceef1cb0e33af33c856363bb
Miguel Cobian <miguelcobian99@gmail.com>
2023-10-02 15:13:05 -0600

    Mock al custom hook

--- __tests__/components/GifGrid.test.jsx
+++ __tests__/components/GifGrid.test.jsx
@@ -2,12 +2,20 @@
 
 import { render, screen } from "@testing-library/react";
 import { GifGrid } from "../../src/components/GifGrid";
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
+
+jest.mock('../../src/hooks/useFetchGifs');
 
 describe('Pruebas en <GifGrid />', () => {
 
     const category = 'Halo';
 
     test('Debe de mostrar el Loading inicialmente', () => {
+
+        useFetchGifs.mockReturnValue({
+            images: [],
+            isLoading: true,
+        });
 
         render(<GifGrid category={category} />);
 
@@ -18,7 +26,27 @@ describe('Pruebas en <GifGrid />', () => {
 
     test('Debe de mostrar items cuando se cargan las imagenes', () => {
 
+        const gifs = [
+            {
+                id: 'ABC',
+                title: 'Saitama',
+                url: 'auuu',
+            },
+            {
+                id: 'ABCD',
+                title: 'Koku',
+                url: 'uwu',
+            },
 
+        ];
+
+        useFetchGifs.mockReturnValue({
+            images: gifs,
+            isLoading: false,
+        });
+
+        render(<GifGrid category={category} />);
+        expect(screen.getAllByRole('img').length).toBe(2);
 
     });
 
