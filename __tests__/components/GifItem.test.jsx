90c004e0f20c1d76e31ad4ca4fd70927f209e935
Miguel Cobian <miguelcobian99@gmail.com>
2023-10-02 12:53:26 -0600

    Demás pruebas al GIfItem

--- __tests__/components/GifItem.test.jsx
+++ __tests__/components/GifItem.test.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { GifItem } from "../../src/components/GifItem";
 
 describe('Pruebas en <GifItem />', () => {
@@ -13,5 +13,20 @@ describe('Pruebas en <GifItem />', () => {
         expect(container).toMatchSnapshot();
 
     });
+
+    test('Debe mostrar la imagen con el URL y el ALT indicado', () => {
+
+        render(<GifItem title={title} url={url} />);
+        // expect(screen.getByRole('img').alt).toBe(title);
+        const { src, alt } = screen.getByRole('img');
+        expect(src).toBe(url);
+        expect(alt).toBe(title);
+
+    });
+
+    test('Debe de mostrar el título en el componente', () => {
+        render(<GifItem title={title} url={url} />);
+        expect(screen.getByText(title)).toBeTruthy();
+    })
 
 })
